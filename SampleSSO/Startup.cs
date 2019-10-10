783b2dacf1d1114b3c5fce84684a57bdb952fd20
Kim, Byong (M CIO:Miracle Systems) <bykim@usaid.gov>
2019-10-10 09:01:46 -0400

    okta poc task-29608

--- SampleSSO/Startup.cs
+++ SampleSSO/Startup.cs
@@ -33,34 +33,71 @@ namespace SampleSSO
                 options.DefaultChallengeScheme = "oidc";
             })
                 .AddCookie("Cookies")
-                //.AddOpenIdConnect("oidc", options =>
-                //{
-                //    options.SignInScheme = "Cookies";
+            //.AddOpenIdConnect("oidc", options =>
+            //{
+            //    options.SignInScheme = "Cookies";
 
-                //    //options.Authority = "https://identityserver-gh.azurewebsites.net/core/";
-                //    options.Authority = "https://prod-identityserver.usaid-apps-east.p.azurewebsites.net/core/";
-                //    options.ClientId = "9fc034a1-4815-469d-8a8f-f7499e05f621";
-                //    //options.Authority = "https://disdev2-identity.usaid.gov/core/";
-                //    //options.ClientId = "C8A30346-BDCD-4D7E-B560-0B50F0075076";
-                //    options.RequireHttpsMetadata = false;
-                //    options.CallbackPath = "/index";
-                //    options.SaveTokens = true;
-                //    options.Scope.Add("roles");
-                //    //options.Scope.Add("upn");
-                //});
+            //    //options.Authority = "https://identityserver-gh.azurewebsites.net/core/";
+            //    options.Authority = "https://prod-identityserver.usaid-apps-east.p.azurewebsites.net/core/";
+            //    options.ClientId = "9fc034a1-4815-469d-8a8f-f7499e05f621";
+            //    //options.Authority = "https://disdev2-identity.usaid.gov/core/";
+            //    //options.ClientId = "C8A30346-BDCD-4D7E-B560-0B50F0075076";
+            //    options.RequireHttpsMetadata = false;
+            //    options.CallbackPath = "/index";
+            //    options.SaveTokens = true;
+            //    options.Scope.Add("roles");
+            //    //options.Scope.Add("upn");
+            //});
+            //.AddOpenIdConnect("oidc", options =>
+            // {
+            //     options.SignInScheme = "Cookies";
+
+            //     //options.Authority = "https://prod-identityserver.usaid-apps-east.p.azurewebsites.net/core/";
+            //     options.Authority = "https://identitydev-test.usaid.gov/";
+            //     options.ClientId = "C8A30346-BDCD-4D7E-B560-0B50F0075076";
+            //     options.RequireHttpsMetadata = false;
+            //     options.CallbackPath = "/Home";
+            //     options.SaveTokens = true;
+            //     //options.Scope.Add("roles");
+            //     //options.Scope.Add("upn");
+            // });
+
+            //IdentityServer
+            //.AddOpenIdConnect("oidc", options =>
+            //{
+            //    options.SignInScheme = "Cookies";
+            //    options.Authority = "https://identitydev.usaid.gov/core/";
+            //    options.ClientId = "9fc034a1-4815-469d-8a8f-f7499e05f621";
+            //    options.RequireHttpsMetadata = false;
+            //    options.CallbackPath = "/Home";
+            //    options.SaveTokens = true;
+            //    options.Scope.Add("roles");
+            //    //options.Scope.Add("upn");
+            //});
+
+            //OKTA
+            //.AddOpenIdConnect("oidc", options =>
+            //{
+            //    options.SignInScheme = "Cookies";
+            //    options.Authority = "https://usaid-eval.okta.com/oauth2/default";
+            //    options.ClientId = "0oa1hn4cac4BZMxnx0h8";
+            //    options.RequireHttpsMetadata = false;
+            //    options.CallbackPath = "/Home";
+            //    options.SaveTokens = true;
+            //    //options.Scope.Add("roles");
+            //    //options.Scope.Add("upn");
+            //});
             .AddOpenIdConnect("oidc", options =>
-             {
-                 options.SignInScheme = "Cookies";
-
-                 //options.Authority = "https://prod-identityserver.usaid-apps-east.p.azurewebsites.net/core/";
-                 options.Authority = "https://identitydev-test.usaid.gov/";
-                 options.ClientId = "C8A30346-BDCD-4D7E-B560-0B50F0075076";
-                 options.RequireHttpsMetadata = false;
-                 options.CallbackPath = "/Home";
-                 options.SaveTokens = true;
-                 //options.Scope.Add("roles");
-                 //options.Scope.Add("upn");
-             });
+            {
+                options.SignInScheme = Configuration.GetSection("AuthenticationServer:SignInScheme").Get<string>();
+                options.Authority = Configuration.GetSection("AuthenticationServer:Authority").Get<string>();
+                options.ClientId = Configuration.GetSection("AuthenticationServer:ClientId").Get<string>();
+                options.RequireHttpsMetadata = Configuration.GetSection("AuthenticationServer:RequireHttpsMetadata").Get<bool>();
+                options.CallbackPath = Configuration.GetSection("AuthenticationServer:CallbackPath").Get<string>();
+                options.SaveTokens = Configuration.GetSection("AuthenticationServer:SaveTokens").Get<bool>();
+                //options.Scope.Add("roles");
+                //options.Scope.Add("upn");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
